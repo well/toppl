4396e6a13a819962354f1ab1557672cd54e82018
Matt Charters <matt@well.ca>
2011-04-09 16:00:11 -0400

    add crummy charts

--- client/lib/toppl.js
+++ client/lib/toppl.js
@@ -14,13 +14,34 @@ $(document).ready(function() {
 	now.setNextVenues = function(venues) {
 		$("#bar-choice-0").html(venues[0].name);
 		$("#venue-choices").html("");
+		
+		var totalVotes = 0;
+		var highestVote = 0;
+		
+		var votes = new Array();
+		
+		var chartVals = "";
+		
 		for(var i in venues) {
 			$("#venue-choices").append('<a href="#" id="venue-' + i +
 									   '" value="' + i +
 									   '">Vote ' + venues[i].name + '</a>' +
 									   ' Current Votes: ' + venues[i].votes +
 									   '<br/>');
+			
+			totalVotes += venues[i].votes;
+			
+			if(venues[i].votes > highestVote) {
+				highestVote = venues[i].votes;
+			}
+			
+			chartVals += (venues[i].votes + "|"); 
 		}
+		
+		chartVals = chartVals.substring(0, chartVals.length - 1);
+		$("#vote-results").html("");
+		$("#vote-results").html('<img src="http://chart.apis.google.com/chart?chxr=0,0,' + highestVote + '?chxt=y&chbh=a&chs=300x225&cht=bvg&chco=A2C180,3D7930&chd=t:' + chartVals + '" width="300" height="225" />');
+		
 		bindVotes();
 	}
 
