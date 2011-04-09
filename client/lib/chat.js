dd65fb2d5ca98fd5580d1e6295547522c4ad3073
Matt Charters <matt@well.ca>
2011-04-09 11:47:43 -0400

    Add chat.js

---
+++ client/lib/chat.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	function sendMessage() {
+		now.distributeMessage($("#text-input").val());
+		$("#text-input").val("");
+		$("#text-input").focus();
+	}
+  
+	now.name = prompt("What's your name?", "");
+	now.receiveMessage = function(name, message){
+		$("#messages").append("<br>" + name + ": " + message);
+	}
+	  
+	$("#send-button").click(function(){
+		sendMessage();
+	});
+	
+	
+});
